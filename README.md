bc7b87bd3ae239b47efa842bb1bc37ea2e8b99e5
Raunak Kedia <raunakkedia1@gmail.com>
2021-08-09 17:21:03 +0530

    Stripe integration when client & server in same url

---
+++ README.md
@@ -0,0 +1,4 @@
+# STRIPE INTEGRATION WITH NODEJS + VANILLA JS (any JS based UI will follow same approach)
+
+- Package used [stripe](https://github.com/stripe/stripe-node).
+- Documentation can be found [here](https://stripe.com/docs/payments/accept-a-payment).
---
+++ server/public/script.js
@@ -0,0 +1,27 @@
+const button = document.querySelector("button");
+
+const buyOrder = [
+	{ id: 1, quantity: 3 },
+	{ id: 2, quantity: 1 },
+];
+
+button.addEventListener("click", async () => {
+	try {
+		const res = await fetch("/create-checkout-session", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				items: buyOrder,
+			}),
+		});
+		if (!res.ok) {
+			throw new Error(
+				`A ${res.status} error has occurred: ${res.statusText}`
+			);
+		}
+		const { url } = await res.json();
+		window.location = url;
+	} catch (e) {
+		console.error(e);
+	}
+});
